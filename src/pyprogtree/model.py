d00bca52a2903e12f6f4464cb264af04c7b0b38b
Luka Janjić <luka.janjic@gmail.com>
2023-05-28 18:52:55 +0200

    implement pushing empties maximally to the left

--- src/pyprogtree/model.py
+++ src/pyprogtree/model.py
@@ -1,6 +1,6 @@
 from math import log, ceil
 from cpmpy import intvar, Model
-from cpmpy.expressions.globalconstraints import Element, Count
+from cpmpy.expressions.globalconstraints import Element, Count, IfThenElse
 from src.pyprogtree.grammar import *
 from src.pyprogtree.plot_tree import plot_tree
 import numpy as np
@@ -15,7 +15,7 @@ def solve(g, min_n, max_n, max_depth=float("inf")):
     :return:
     """
 
-    max_depth = min(max_n - 1, max_depth)
+    max_depth = min(max_n, max_depth+1)
 
     # Todo: export fields from the (julia) csg struct to a python class that holds:
     #    * TYPES
@@ -36,7 +36,7 @@ def solve(g, min_n, max_n, max_depth=float("inf")):
     arity = intvar(0, g.MAX_ARITY, shape = max_n, name="Arity")
     child_index = intvar(1-max_n+min_n, g.MAX_ARITY-1, shape = max_n, name="ChildIndex")
     init_index = intvar(0, max_n-min_n, name="InitialIndex")
-    ancestor_path = intvar(0, g.MAX_ARITY, shape = (max_n, max_depth), name="AncestorPath")
+    ancestor_path = intvar(1-max_n+min_n, g.MAX_ARITY, shape = (max_n, max_depth), name="AncestorPath")
 
     base = np.array([(g.MAX_ARITY + 1) ** i for i in range(max_depth)][::-1])
 
@@ -49,32 +49,32 @@ def solve(g, min_n, max_n, max_depth=float("inf")):
         # node is empty iff its before the initial index
         [(n < init_index) == (rule[n] == g.EMPTY_RULE) for n in range(max_n)],
 
-        # True non-root nodes are 1 more deep than their parents
-        [(n >= init_index).implies(depth[n] == depth[parent[n]] + 1) for n in range(max_n - 1)],
-        # Empty nodes have depth 1
-        #[(n < init_index).implies(depth[n] == 1) for n in range(max_n - 1)],
+        # Non-root nodes are 1 more deep than their parents
+        [depth[n] == depth[parent[n]] + 1 for n in range(max_n - 1)],
 
         # Enforcing the arity according to the tree structure
-        # -- remove impl?
-        [(n >= init_index).implies(arity[n] == Count(parent, n)) for n in range(max_n-1)],
-        
+        [
+            IfThenElse(init_index == n, 
+                arity[n] == Count(parent, n) - init_index,
+                arity[n] == Count(parent, n)
+            ) for n in range(max_n)
+        ],
+
         # Empties are leftmost tail
-        #[(n < init_index).implies(parent[n] == init_index) for n in range(max_n-1)],
+        [(n < init_index).implies(parent[n] == init_index) for n in range(max_n-1)],
 
         # Enforcing the arity according to the number of children per rule
         # Note that '>=' is used instead of the expected '=='
         # This is such that empty rule nodes can freely be added as children
         # This makes it such that we can also find tree with less than max_n nodes
         # We might want to find another way of reducing the number of nodes in the future
-        # -- remove impl?
-        [(n >= init_index).implies(arity[n] == Element(g.RULE_ARITY, rule[n])) for n in range(max_n)],
+        [arity[n] == Element(g.RULE_ARITY, rule[n]) for n in range(max_n)],
 
         # Indexing children of the same parent
         (init_index == 0).implies(child_index[0] == 0),
         [(n >= init_index).implies(child_index[n] == Count(parent[:n], parent[n])) for n in range(1, max_n-1)],
-        
         # Child index of empty nodes is negative
-        #[(n < init_index).implies(child_index[n] == n - init_index) for n in range(0, max_n-1)],
+        [(n < init_index).implies(child_index[n] == n - init_index) for n in range(0, max_n-1)],
 
         # Enforce the children of each node are of the correct type: TYPES[rule[n]] == CHILD_TYPES[rule[parent[n]], child_index[n]]
         [
@@ -87,23 +87,20 @@ def solve(g, min_n, max_n, max_depth=float("inf")):
         # Fix ancestor path of the root
         [ancestor_path[max_n-1, d] == g.MAX_ARITY for d in range(max_depth)],
         
-        # Pad ancestor path of empty nodes with 0s 
-        [(n < init_index).implies(sum(ancestor_path[n]) == 0) for n in range(max_n-1)],
-
-        # Enforce each true node's path to be an extension of its parents path 
+        # Enforce each node's path to be an extension of its parents path 
         [
-            ((n >= init_index) & (d < depth[n]-1)).implies(
+            (d < depth[n]-1).implies(
                    ancestor_path[n, d] 
                 == ancestor_path[parent[n], d]) 
             for n in range(max_n-1) for d in range(max_depth)
         ],
 
         # Enforce each non-root node's last path symbol to be its child index
-        [(n >= init_index).implies(ancestor_path[n, depth[n]-1] == child_index[n]) for n in range(max_n-1)],
+        [ancestor_path[n, depth[n]-1] == child_index[n] for n in range(max_n-1)],
 
         # Enforce the remaining path symbols to be max_arity
         [
-            ((n >= init_index) & (d >= depth[n])).implies(
+            ((d >= depth[n])).implies(
                 ancestor_path[n, d] == g.MAX_ARITY
             ) 
             for n in range(max_n-1) for d in range(max_depth)
