3dbf1f4d230d2a44ad1d24b3359b611e81361ae0
Luka Janjić <luka.janjic@gmail.com>
2023-06-07 20:26:33 +0200

    add index computing function for children

--- src/pyprogtree/constraints/enforce_children.py
+++ src/pyprogtree/constraints/enforce_children.py
@@ -1,10 +1,11 @@
-from src.pyprogtree.decision_variables import DecisionVariables
+def index(dv: DecisionVariables, node_index, child_index):
+    return node_index*dv.g.MAX_ARITY + child_index
 
 def enforce_children(dv: DecisionVariables):
     """
     Enforces children[n,i] to contain the ith child of node n
     """
     return [
-        [dv.children[dv.parent[n]*dv.g.MAX_ARITY + dv.child_index[n]] == n  for n in range(dv.max_n-1)],
-        [(i >= dv.arity[n]).implies(dv.children[n*dv.g.MAX_ARITY + i] == 0) for n in range(dv.max_n-1) for i in range(dv.g.MAX_ARITY)]
+        [dv.children[index(dv, dv.parent[n], dv.child_index[n])] == n  for n in range(dv.max_n-1)],
+        [(i >= dv.arity[n]).implies(dv.children[index(dv, n, i)] == 0) for n in range(dv.max_n-1) for i in range(dv.g.MAX_ARITY)]
     ]
