fd898ad6d82a126ea2caffe9686623cc5206a174
Whebon <bart-man99@hotmail.com>
2023-06-15 14:45:18 +0200

    Timeit returns value

--- src/pyprogtree/pyprogtree/timeit.py
+++ src/pyprogtree/pyprogtree/timeit.py
@@ -3,7 +3,8 @@ import time
 def timeit(f):
     def inner(*args, **kwargs):
         start = time.time()
-        f(*args, **kwargs)
+        retval = f(*args, **kwargs)
         end = time.time()
         print(f"Took {end-start} seconds")
+        return retval
     return inner
