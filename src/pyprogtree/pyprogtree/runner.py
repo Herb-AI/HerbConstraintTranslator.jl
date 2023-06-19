c3e89e57041a24fd39820c93d6f58dd1cdac561b
Luka Janjić <luka.janjic@gmail.com>
2023-06-19 15:32:05 +0200

    change default solution limit to None

--- src/pyprogtree/pyprogtree/runner.py
+++ src/pyprogtree/pyprogtree/runner.py
@@ -5,7 +5,7 @@ from pyprogtree.timeit import *
 
 @timeit
 def run(ruletypes, childtypes, typenames, rulenames, constraints, 
-        min_nodes=1, max_nodes=5, max_depth=4, solution_limit=1000, plot_solutions=False):
+        min_nodes=1, max_nodes=5, max_depth=4, solution_limit=None, plot_solutions=False):
     # Convert input ndarrays to python lists
     ruletypes  = list(ruletypes)
     childtypes = list(map(list, childtypes))
