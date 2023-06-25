784852ba8489e0298d5f40f26119d192edf0aee4
Luka Janjić <luka.janjic@gmail.com>
2023-06-25 20:13:27 +0200

    remake init_env

--- src/init_env.jl
+++ src/init_env.jl
@@ -1,7 +1,8 @@
-# replace this with you own conda pyprogtree location
-# build_location = "D:\\ProgramData\\Miniconda3\\Lib\\site-packages\\pyprogtree\\"
+import Pkg
 
-# A series of bash comands to set up the python environment.
-Base.run(`$(PyCall.python) -m pip uninstall -y pyprogtree`) # the last line should be enough now 
-Base.run(`$(PyCall.python) -m pip install --upgrade --force-reinstall  HerbConstraintTranslator.jl\\src\\pyprogtree`) # Not sure how this would work in general, since the path is relative to the root module Herb.jl
-# Base.run(`xcopy .\\HerbConstraintTranslator.jl\\src\\pyprogtree\\pyprogtree\\ $(build_location) /E /C /H /R /K /O /Y`)
+Pkg.add("PyCall")
+
+using PyCall
+
+Base.run(`$(PyCall.python) -m pip install cpmpy matplotlib networkx`)
+Base.run(`$(PyCall.python) -m pip install HerbConstraintTranslator.jl\\src\\pyprogtree`)
